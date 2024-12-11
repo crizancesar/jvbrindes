a7b43029977bead8e9a021e2dcb3b18b09387f65
crizancesar <crizan@icloud.com>
2024-12-11 08:51:35 -0300

    add pixel facebook

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,33 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+require '../assets/vendor/PHPMailer/src/Exception.php';
+require '../assets/vendor/PHPMailer/src/PHPMailer.php';
+require '../assets/vendor/PHPMailer/src/SMTP.php';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $mail = new PHPMailer;
+  $mail->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $mail->to = $receiving_email_address;
+  $mail->from_name = $_POST['name'];
+  $mail->from_email = $_POST['email'];
+  $mail->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+  
+  $mail->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'cdicrizancesar@gmail.com',
+    'password' => 'ciscov-vevfy0-xikcyH',
     'port' => '587'
   );
-  */
+ 
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $mail->add_message( $_POST['name'], 'From');
+  $mail->add_message( $_POST['email'], 'Email');
+  $mail->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $mail->send();
 ?>
